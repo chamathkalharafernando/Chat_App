0e862f141c62c7e4a11ea9353a069e82f43757c3
Chamath Fernando <chamadula555@gmail.com>
2025-02-20 09:59:08 +0530

    first commit

---
+++ lib/Screens/Homescreen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller; // Use 'late' to avoid nullability issues
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsApp"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "CHATS"),
+            Tab(text: "STATUS"),
+            Tab(text: "CALLS"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+          Center(child: Icon(Icons.camera_alt, size: 50)),
+          Center(child: Text("Chats Page")),
+          Center(child: Text("Status Page")),
+          Center(child: Text("Calls Page")),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app/screens/Homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.teal,
+        ).copyWith(
+          secondary: Color(0xFF128C7E),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF075E54), // Apply primary color to AppBar
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+      ),
+      home: Homescreen(),
+    );
+  }
+}
